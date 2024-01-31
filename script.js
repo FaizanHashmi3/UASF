fa66994cb7970e14c3f40cf24b4a3197bf265bcf
FaizanHashmi3 <mdfaizanhashmi3@gmail.com>
2024-01-31 11:01:44 +0530

    changes made on responsive

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function animateValue(obj, start, end, duration) {
     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
     obj.innerHTML = Math.floor(progress * (end - start) + start);
     if (progress < 1) {
-      window.requestAnimationFrame(step);
+      window.requestAnimationFrame(step); 
     }
   };
   window.requestAnimationFrame(step);
